e8740477cddbd27ab30b7452dc3ed9e31b325efd
Soliev <abduvositsoliev@gmail.com>
2023-09-06 09:47:24 +0900

    add jump to start

--- lib/main.dart
+++ lib/main.dart
@@ -47,27 +47,52 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         actions: [
+          JumpButton(controller: _controller),
           IconButton(
-            tooltip: 'Jump to middle',
-            onPressed: () =>
-                _controller.jumpTo(_controller.position.maxScrollExtent / 2),
-            icon: const Icon(Icons.skip_next),
-          ),
-          IconButton(
+            tooltip: 'Sort by Date',
             onPressed: () => context.read<PersonCubit>().sortByDate(),
             icon: const Icon(Icons.date_range),
           ),
           IconButton(
+            tooltip: 'Sort by Name',
             onPressed: () => context.read<PersonCubit>().sortByName(),
             icon: const Icon(Icons.person),
           ),
           IconButton(
+            tooltip: 'Sort by balance',
             onPressed: () => context.read<PersonCubit>().sortByBalance(),
             icon: const Icon(Icons.attach_money),
           ),
         ],
       ),
       body: PersonList(controller: _controller),
+    );
+  }
+}
+
+class JumpButton extends StatefulWidget {
+  const JumpButton({super.key, required this.controller});
+  final ScrollController controller;
+
+  @override
+  State<JumpButton> createState() => _JumpButtonState();
+}
+
+class _JumpButtonState extends State<JumpButton> {
+  bool isMiddle = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        isMiddle
+            ? widget.controller
+                .jumpTo(widget.controller.position.minScrollExtent)
+            : widget.controller
+                .jumpTo(widget.controller.position.maxScrollExtent / 2);
+        setState(() => isMiddle = !isMiddle);
+      },
+      icon: Icon(isMiddle ? Icons.skip_previous : Icons.skip_next),
     );
   }
 }
